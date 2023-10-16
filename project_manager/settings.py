eba15f3767fd6ed43b0a3ae349a492939da17182
stefanias05 <stefaniasoare9@gmail.com>
2023-10-16 21:48:30 +0300

    commit

--- project_manager/settings.py
+++ project_manager/settings.py
@@ -136,11 +136,11 @@ SESSION_SAVE_EVERY_REQUEST = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # SMTP Config
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'managerproject434@gmail.com'
-EMAIL_HOST_PASSWORD = 'jcey fskd wyok jfbn'
+#
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'managerproject434@gmail.com'
+# EMAIL_HOST_PASSWORD = 'jcey fskd wyok jfbn'
 
