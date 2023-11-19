8da78eb138bd725826cfe7075d6d90a06d84c70e
stefanias05 <stefaniasoare9@gmail.com>
2023-11-19 12:08:54 +0200

    commit

--- project_manager/urls.py
+++ project_manager/urls.py
@@ -45,5 +45,6 @@ urlpatterns = [
                   ),
                   path('', include('django.contrib.auth.urls')),
                   path('', include('members.urls')),
-                  path('', include('tasks.urls'))
+                  path('', include('tasks.urls')),
+                  path('', include('notifications.urls'))
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
